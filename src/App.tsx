f0e721c475b0520a34faaa9b2be87fff83f5d094
Shatha-bk <sh.bakhour@marfud.com>
2024-12-11 15:47:22 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Container from "./components/Container";
+
+function App() {
+
+
+  return (
+<main>
+  <Header />
+<Container>
+  <p>Text</p>
+</Container>
+  <Footer />
+</main>
+  )
+}
+
+export default App
